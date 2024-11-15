16e6bb99b9279b712d457206dff32edd973f7ccb
Felix Bonet <fbonet.dam@institutcampalans.net>
2024-11-15 19:08:16 +0100

    Finestra responsables nova (no funcional ni estructurada)

--- ProjecteKanBan/MainWindow.xaml.cs
+++ ProjecteKanBan/MainWindow.xaml.cs
@@ -173,8 +173,10 @@ namespace ProjecteKanBan
 
         private void btnGestionarResponsables_Click(object sender, RoutedEventArgs e)
         {
-            Window w2 = new Window();
-            w2.Show();
+            Window responsables = new Responsables();
+
+            responsables.Show();
+            
         }
     }
 }
